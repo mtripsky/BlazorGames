7fd7eddc93a04f3a2eb063f49321f13f9e65765b
Unknown <m.tripsky@gmail.com>
2020-02-10 10:11:58 +0100

    Refactor infrastructure for BlazorGames

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Minesweeper.Data;
 
-namespace Minesweeper
+namespace BlazorGames
 {
     public class Startup
     {
@@ -28,7 +21,6 @@ namespace Minesweeper
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
